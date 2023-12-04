be50be4e97e331c06157c14180c40c24e5cd6685
Gastón Frenkel <gst.frenkel@gmail.com>
2023-12-04 20:09:40 -0300

    semicolons

--- test/singleton.js
+++ test/singleton.js
@@ -1,17 +1,17 @@
-const { mockDeep, mockReset } = require('jest-mock-extended')
+const { mockDeep, mockReset } = require('jest-mock-extended');
 
 const prisma = require('../src/client');
 const axios = require('axios');
 
-const prismaMock = prisma
-const axiosMock = axios
+const prismaMock = prisma;
+const axiosMock = axios;
 
-jest.mock('../src/client', () => mockDeep())
-jest.mock('axios', () => mockDeep())
+jest.mock('../src/client', () => mockDeep());
+jest.mock('axios', () => mockDeep());
 
 beforeEach(() => {
   	mockReset(prismaMock);
   	mockReset(axiosMock);
 })
 
-module.exports = {prismaMock, axiosMock}
+module.exports = {prismaMock, axiosMock};
